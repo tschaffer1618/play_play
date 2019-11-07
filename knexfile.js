cfd8688ca5b263fdbc3ae42ffad75bf64d33a734
Cory Westerfield <cory.westerfield@gmail.com>
2019-11-07 14:29:51 -0700

    Remove reference to pg database on Heroku

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://sasczswyekkqas:9d3367042c8739f39cf869df4ab031fdb653d8381e2302e3a109e7cdc566637c@ec2-174-129-253-53.compute-1.amazonaws.com:5432/d4oqepk9oarmc5',
+    connection: '',
     migrations: {
       directory: './db/migrations'
     },
