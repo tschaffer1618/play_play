61a48c39d9066afe83cf2e349eee821b44bc03c5
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-02 15:43:10 -0700

    Set up production db
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: '',
+    connection: 'postgres://cnmweupyhagrfn:84fc3729f018ad54e571aec70b361fd17d1eba601666937fec30d98f6c15eb65@ec2-54-225-195-3.compute-1.amazonaws.com:5432/d3qpnkg3hf64pd',
     migrations: {
       directory: './db/migrations'
     },
