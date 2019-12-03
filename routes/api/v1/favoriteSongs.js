1a10201747193cd64186a446399dba6c9cd3530d
Leiya Kenney <leiya.kenney@gmail.com>
2019-12-02 22:29:55 -0700

    Create delete method for favorite songs based on songId

--- routes/api/v1/favoriteSongs.js
+++ routes/api/v1/favoriteSongs.js
@@ -16,4 +16,22 @@ router.get('/', (request, response) => {
     });
 });
 
+router.delete("/", (request, response) => {
+  const favorite = request.body;
+  const songId = favorite.id;
+  const songName = favorite.title;
+
+  database("favorite_songs").where("id", songId)
+    .then(favorite => {
+      if (favorite[0]) {
+        database("favorite_songs").del().where({ id: songId })
+          .then(favorite => {
+            response.status(204).json({ message: `Song with id ${songId} has been removed from your favorites`});
+          });
+      } else {
+        return response.status(404).json({ error: `No song found in favorites with id ${songId}.` });
+      }
+    });
+});
+
 module.exports = router;
