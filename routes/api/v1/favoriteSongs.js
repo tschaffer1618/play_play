49ae03da438fdc98abc9710e45c2bc4be0a9be56
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-02 22:00:13 -0700

    Add sad path for getting a favorite song by id

--- routes/api/v1/favoriteSongs.js
+++ routes/api/v1/favoriteSongs.js
@@ -9,7 +9,11 @@ const getFavoriteSong = router.get('/:id', (request, response) => {
   const favoriteId = request.params.id
   database('favorite_songs').where('id', favoriteId)
     .then((favoriteSong) => {
-      response.status(200).json(favoriteSong);
+      if (favoriteSong[0]) {
+        response.status(200).json(favoriteSong);
+      } else {
+        response.status(404).json({ error: 'Song not found'});
+      }
     })
     .catch((error) => {
       response.status(500).json({ error });
