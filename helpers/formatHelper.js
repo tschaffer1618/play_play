b4bddb83697cdcbce3fb23dd04470047039e5675
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-03 09:54:05 -0700

    Make minor edit to song format to put it in array

--- helpers/formatHelper.js
+++ helpers/formatHelper.js
@@ -1,11 +1,11 @@
 function formatSong(song) {
-  var formattedSong = {
+  var formattedSong = [{
     id: song[0].id,
     title: song[0].title,
     artistName: song[0].artist_name,
     genre: song[0].genre,
     rating: song[0].rating
-  };
+  }];
   return formattedSong;
 }
 
