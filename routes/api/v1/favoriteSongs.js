1d2ae5752ab659e03672d50bb0016a60e014170b
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-02 21:49:04 -0700

    Get a favorite song by id

--- routes/api/v1/favoriteSongs.js
+++ routes/api/v1/favoriteSongs.js
@@ -5,15 +5,17 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
-
-router.get('/', (request, response) => {
-  database('favorite_songs').select()
-    .then((favoriteSongs) => {
-      response.status(200).json(favoriteSongs);
+const getFavoriteSong = router.get('/:id', (request, response) => {
+  const favoriteId = request.params.id
+  database('favorite_songs').where('id', favoriteId)
+    .then((favoriteSong) => {
+      response.status(200).json(favoriteSong);
     })
     .catch((error) => {
       response.status(500).json({ error });
     });
 });
 
-module.exports = router;
+module.exports = {
+  getFavoriteSong
+};
