f0b43a308f4404680ba5cdcabe06199890788f2e
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-04 11:51:12 -0700

    Rename database env variable
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: `${process.env.DATABASE_CONN}`,
+    connection: `${process.env.DATABASE_URL}`,
     migrations: {
       directory: './db/migrations'
     },
