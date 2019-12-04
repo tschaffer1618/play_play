173b0d54d4e5018376ea4302211b595f23cafe45
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-04 11:45:55 -0700

    Use env variable to specify db connection in production
    Co-authored-by: Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -21,17 +21,9 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-  staging: {
-    client: 'pg',
-    connection: 'postgres://pcfnpbacvozmev:a5473d00ab934d6fd7bc72f93a6bd3401530fe53cb9f4832a9968fc0ad44d774@ec2-54-227-249-202.compute-1.amazonaws.com:5432/dcam6dsddarvjb',
-    migrations: {
-      directory: './db/migrations'
-    },
-    useNullAsDefault: true
-  },
   production: {
     client: 'pg',
-    connection: 'postgres://cnmweupyhagrfn:84fc3729f018ad54e571aec70b361fd17d1eba601666937fec30d98f6c15eb65@ec2-54-225-195-3.compute-1.amazonaws.com:5432/d3qpnkg3hf64pd',
+    connection: `${process.env.DATABASE_CONN}`,
     migrations: {
       directory: './db/migrations'
     },
