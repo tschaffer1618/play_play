c299e6fad784a3b7e5c041091e9d7e1be9fbc096
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-05 15:12:33 -0700

    Test getting all playlists

--- tests/playlists.spec.js
+++ tests/playlists.spec.js
@@ -26,5 +26,26 @@ describe('Test the playlists path', () => {
     database.raw('truncate table playlists cascade');
   });
 
-  //tests here
+  describe('test playlists GET all', () => {
+    it('happy path', async () => {
+      const res = await request(app)
+        .get(`/api/v1/playlists`);
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBe(2);
+
+      expect(res.body[0]).toHaveProperty('id');
+      expect(res.body[1]).toHaveProperty('id');
+
+      expect(res.body[0]).toHaveProperty('title');
+      expect(res.body[0].title).toBe('Test Playlist');
+      expect(res.body[1].title).toBe('Other Test Playlist');
+
+      expect(res.body[0]).toHaveProperty('createdAt');
+      expect(res.body[1]).toHaveProperty('createdAt');
+
+      expect(res.body[0]).toHaveProperty('updatedAt');
+      expect(res.body[1]).toHaveProperty('updatedAt');
+    });
+  });
 })
