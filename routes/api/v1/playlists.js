f0ebf87b30ef0e6218bdf4fae97319df5d73217b
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-06 08:56:33 -0700

    Build functionality for a user editing the title of a playlist

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -18,6 +18,32 @@ const getAllPlaylists = router.get('/', (request, response) => {
     });
 });
 
+const editPlaylist = router.put('/:id', (request, response) => {
+  const playlistId = request.params.id;
+  const newTitle = request.body.title;
+  database('playlists').where({title: `${newTitle}`})
+    .then((duplicatePlaylist) => {
+      if (duplicatePlaylist[0]) {
+        response.status(400).send({error: `You already have a playlist called ${newTitle}.`})
+      } else {
+        database('playlists').where({id: `${playlistId}`})
+          .then((playlist) => {
+            if (playlist[0]) {
+              database('playlists').where({id: `${playlistId}`}).update({title: `${newTitle}`})
+                .then((updatedPlaylist) => {
+                  database('playlists').where({id: `${playlistId}`}).select('id', 'title', 'created_at as createdAt', 'updated_at as updatedAt')
+                    .then((playlistToSend) => {
+                      response.status(200).json(playlistToSend[0])
+                    })
+                })
+            } else {
+              response.status(404).send({error: 'No playlist found matching that id. Try again!'})
+            }
+          })
+      }
+    })
+})
+
 module.exports = {
   getAllPlaylists
 }
