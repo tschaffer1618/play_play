6b21514938c9966818dd0b1742830580fcd3da48
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-06 13:51:32 -0700

    Test the edit playlist method (happy path and 2 sad paths)

--- tests/playlists.spec.js
+++ tests/playlists.spec.js
@@ -108,4 +108,60 @@ describe('Test the playlists path', () => {
       expect(res2.body.message).toBe("You already have a playlist called Party!")
     })
   });
+
+  describe('test playlist PUT', () => {
+    it('can update a playlist', async () => {
+      const playlist = await database('playlists')
+        .where('title', 'Test Playlist').select('id');
+      const playlistId = playlist[0].id;
+
+      const body = {
+        "playlistTitle": "Party"
+      }
+
+      const res = await request(app)
+        .put(`/api/v1/playlists/${playlistId}`)
+        .send(body)
+
+      expect(res.statusCode).toBe(200);
+
+      expect(res.body).toHaveProperty('title');
+      expect(res.body.title).toBe("Party");
+    });
+
+    it("can't update a playlist with a duplicate name", async () => {
+      const playlist = await database('playlists')
+        .where('title', 'Test Playlist').select('id');
+      const playlistId = playlist[0].id;
+
+      const body = {
+        "playlistTitle": "Other Test Playlist"
+      }
+
+      const res = await request(app)
+        .put(`/api/v1/playlists/${playlistId}`)
+        .send(body)
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.message).toBe("You already have a playlist called Other Test Playlist!")
+    })
+
+    it("can't update a playlist if an incorrect id is sent", async () => {
+      const playlist = await database('playlists')
+        .where('title', 'Test Playlist').select('id');
+      const playlistId = playlist[0].id;
+      const wrongId = playlistId + 10;
+
+      const body = {
+        "playlistTitle": "Random Name"
+      }
+
+      const res = await request(app)
+        .put(`/api/v1/playlists/${wrongId}`)
+        .send(body)
+
+      expect(res.statusCode).toBe(404);
+      expect(res.body.error).toBe("No playlist found matching that id. Try again!")
+    })
+  });
 })
