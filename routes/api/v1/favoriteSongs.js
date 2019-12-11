4902fbd00420571b54b916599157c5082ad21663
Leiya Kenney <leiya.kenney@gmail.com>
2019-12-11 08:45:50 -0700

    Refactor getFavoriteSong

--- routes/api/v1/favoriteSongs.js
+++ routes/api/v1/favoriteSongs.js
@@ -8,23 +8,18 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
-const getFavoriteSong = router.get('/:id', (request, response) => {
-  const favoriteId = request.params.id
-  database('favorite_songs').where('id', favoriteId)
-    .then((favoriteSong) => {
-      if (favoriteSong[0]) {
-        response.status(200).send(formatHelper.formatSong(favoriteSong));
-      } else {
-        response.status(404).json({ error: 'Song not found'});
-      }
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    });
-});
+const getFavoriteSong = router.get("/:id", async (request, response) => {
+  const favoriteId = request.params.id;
+  const favoriteSong = await database("favorite_songs").where("id", favoriteId);
+  if (favoriteSong[0]) {
+    response.status(200).send(formatHelper.formatSong(favoriteSong));
+  } else {
+    response.status(404).json({ error: 'Song not found'});
+  }
+})
 
-const getAllFavoriteSongs = router.get('/', async (request, response) => {
-  const favSongs = await database('favorite_songs').select("id", "title", "artist_name as artistName", "genre", "rating")
+const getAllFavoriteSongs = router.get("/", async (request, response) => {
+  const favSongs = await database("favorite_songs").select("id", "title", "artist_name as artistName", "genre", "rating")
   response.status(200).send(favSongs)
 })
 
