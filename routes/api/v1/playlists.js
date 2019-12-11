06705a630aff01aec331fe60e0af2261a20714d9
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-10 21:51:58 -0700

    Replace all single quotes with double quotes

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 var formatHelper = require("../../../helpers/formatHelper");
 var apiHelper = require("../../../helpers/apiHelper");
 var databaseHelper = require("../../../helpers/databaseHelper");
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../../knexfile')[environment];
-const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("../../../knexfile")[environment];
+const database = require("knex")(configuration);
 
 const deletePlaylist = router.delete("/:id", async (request, response) => {
   const playlistId = request.params.id;
@@ -32,21 +32,21 @@ const createPlaylist = router.post("/", async (request, response) => {
     response.status(400).json({ message: `You already have a playlist called ${title}!`});
   } else {
     const newPlaylist = await database("playlists").insert({title: title})
-      .returning(['id', 'title', 'created_at as createdAt', 'updated_at as updatedAt']);
+      .returning(["id", "title", "created_at as createdAt", "updated_at as updatedAt"]);
     response.status(201).json(newPlaylist);
   }
 });
 
-const getPlaylist = router.get('/:id/favorites', async (request, response) => {
+const getPlaylist = router.get("/:id/favorites", async (request, response) => {
   const playlistId = request.params.id
   const favorites = await databaseHelper.getPlaylistFavorites(playlistId);
-  const existingPlaylist =  await database('playlists').where({id: playlistId})
-    .select('id', 'title', 'created_at as createdAt', 'updated_at as updatedAt')
+  const existingPlaylist =  await database("playlists").where({id: playlistId})
+    .select("id", "title", "created_at as createdAt", "updated_at as updatedAt")
   if (existingPlaylist[0] && favorites[0]) {
-    const playlist = await database('playlists')
-      .innerJoin('playlist_songs', 'playlists.id', 'playlist_songs.playlist_id')
-      .where("playlists.id", playlistId )
-      .select('playlists.id', 'title', 'playlists.created_at as createdAt', 'playlists.updated_at as updatedAt')
+    const playlist = await database("playlists")
+      .innerJoin("playlist_songs", "playlists.id", "playlist_songs.playlist_id")
+      .where("playlists.id", playlistId)
+      .select("playlists.id", "title", "playlists.created_at as createdAt", "playlists.updated_at as updatedAt")
     response.status(200).send(formatHelper.formatPlaylist(playlist, favorites));
   } else if (existingPlaylist[0]) {
     response.status(200).send(formatHelper.formatPlaylist(existingPlaylist, favorites));
@@ -55,29 +55,29 @@ const getPlaylist = router.get('/:id/favorites', async (request, response) => {
   }
 });
 
-const getAllPlaylists = router.get('/', async (request, response) => {
-  const playlists = await database('playlists').select('id', 'title', 'created_at as createdAt', 'updated_at as updatedAt')
+const getAllPlaylists = router.get("/", async (request, response) => {
+  const playlists = await database("playlists").select("id", "title", "created_at as createdAt", "updated_at as updatedAt")
   const final = await formatHelper.getAllFavorites(playlists);
   response.status(200).send(final);
 });
 
-const editPlaylist = router.put('/:id', (request, response) => {
+const editPlaylist = router.put("/:id", async (request, response) => {
   const playlistId = request.params.id;
   const newTitle = request.body.playlistTitle;
-  database('playlists').where({title: `${newTitle}`})
+  database("playlists").where({title: `${newTitle}`})
     .then((duplicatePlaylist) => {
       if (duplicatePlaylist[0]) {
         response.status(400).send({message: `You already have a playlist called ${newTitle}!`})
       } else {
-        database('playlists').where({id: `${playlistId}`})
+        database("playlists").where({id: `${playlistId}`})
           .then((playlist) => {
             if (playlist[0]) {
-              database('playlists').where({id: `${playlistId}`}).update({title: `${newTitle}`}).returning(['id', 'title', 'created_at as createdAt', 'updated_at as updatedAt'])
+              database("playlists").where({id: `${playlistId}`}).update({title: `${newTitle}`}).returning(["id", "title", "created_at as createdAt", "updated_at as updatedAt"])
                 .then((updatedPlaylist) => {
                   response.status(200).json(updatedPlaylist[0])
                 })
             } else {
-              response.status(404).send({error: 'No playlist found matching that id. Try again!'})
+              response.status(404).send({error: "No playlist found matching that id. Try again!"})
             }
           })
       }
