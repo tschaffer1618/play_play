583ab64571fa2a9b6988c597659a7141c3578c85
Leiya Kenney <leiya.kenney@gmail.com>
2019-12-11 11:57:58 -0700

    Reorder methods to create, get, getAll, delete for consistency

--- routes/api/v1/favoriteSongs.js
+++ routes/api/v1/favoriteSongs.js
@@ -8,32 +8,6 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
-const getFavoriteSong = router.get("/:id", async (request, response) => {
-  const favoriteId = request.params.id;
-  const favoriteSong = await database("favorite_songs").where("id", favoriteId);
-  if (favoriteSong[0]) {
-    response.status(200).send(formatHelper.formatSong(favoriteSong));
-  } else {
-    response.status(404).json({ error: 'Song not found'});
-  }
-})
-
-const getAllFavoriteSongs = router.get("/", async (request, response) => {
-  const favSongs = await database("favorite_songs").select("id", "title", "artist_name as artistName", "genre", "rating")
-  response.status(200).send(favSongs)
-});
-
-const deleteFavoriteSong = router.delete("/:id", async (request, response) => {
-  const songId = request.params.id;
-  const favorite = await database("favorite_songs").where( "id", songId )
-  if (favorite[0]) {
-    const delSong = await database("favorite_songs").del().where({ id: songId })
-    response.status(204).send();
-  } else {
-    return response.status(404).json({ error: "Song not found" });
-  }
-});
-
 const createFavoriteSong = router.post("/", async (request, response) => {
   const title = request.body.title;
   const artist = request.body.artistName;
@@ -61,9 +35,36 @@ const createFavoriteSong = router.post("/", async (request, response) => {
   }
 });
 
+const getFavoriteSong = router.get("/:id", async (request, response) => {
+  const favoriteId = request.params.id;
+  const favoriteSong = await database("favorite_songs").where("id", favoriteId);
+  if (favoriteSong[0]) {
+    response.status(200).send(formatHelper.formatSong(favoriteSong));
+  } else {
+    response.status(404).json({ error: 'Song not found'});
+  }
+})
+
+const getAllFavoriteSongs = router.get("/", async (request, response) => {
+  const favSongs = await database("favorite_songs").select("id", "title", "artist_name as artistName", "genre", "rating")
+  response.status(200).send(favSongs)
+});
+
+const deleteFavoriteSong = router.delete("/:id", async (request, response) => {
+  const songId = request.params.id;
+  const favorite = await database("favorite_songs").where( "id", songId )
+  if (favorite[0]) {
+    const delSong = await database("favorite_songs").del().where({ id: songId })
+    response.status(204).send();
+  } else {
+    return response.status(404).json({ error: "Song not found" });
+  }
+});
+
+
 module.exports = {
+  createFavoriteSong,
   getFavoriteSong,
   getAllFavoriteSongs,
-  deleteFavoriteSong,
-  createFavoriteSong
+  deleteFavoriteSong
 };
